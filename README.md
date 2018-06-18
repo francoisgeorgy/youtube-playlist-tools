c3c14bcf1aea5090e510b3626e5d5fe38c09afe5
François Georgy <francois.georgy@gmail.com>
2018-06-18 12:47:41 +0200

    testing...

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ Requisite:
 
 - Youtube DATA API access : https://console.developers.google.com/projectselector/apis/api/youtube.googleapis.com/overview
   - get credentials to access User Data
+
+Resources:
+
+- https://developers.google.com/youtube/v3/quickstart/python
+- http://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.flow.html
+- https://developers.google.com/api-client-library/python/guide/aaa_oauth
+- https://developers.google.com/api-client-library/python/start/installation
+- https://developers.google.com/youtube/v3/docs/
+- https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
   
+Notes:
+
+    sudo -E pip3 install --upgrade google-api-python-client
+    
---
+++ src/list-playlist.py
@@ -0,0 +1,46 @@
+# Sample Python code for user authorization
+
+import os
+
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "../secret/client_id.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+
+def get_authenticated_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_console()
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def playlists_list_mine(client, **kwargs):
+    # kwargs = remove_empty_kwargs(**kwargs)
+    response = client.playlists().list(**kwargs).execute()
+    print(response)
+
+
+if __name__ == '__main__':
+    # When running locally, disable OAuthlib's HTTPs verification. When
+    # running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    service = get_authenticated_service()
+    playlists_list_mine(service,
+                        part='snippet,contentDetails',
+                        mine=True,
+                        maxResults=25)
+    # ,
+    #                 onBehalfOfContentOwner='',
+    #                 onBehalfOfContentOwnerChannel='')
